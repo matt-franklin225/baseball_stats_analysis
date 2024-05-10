577b88612bbd3965818614b395c101b68b5af4fa
Matt <137856149+matt-franklin225@users.noreply.github.com>
2024-05-09 20:11:44 -0700

    Added CORS fix to the app.py file.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from player_search import search_player
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/search', methods=['POST'])
 def search():
